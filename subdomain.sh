a83140ec1228814c46efa6782302964d0c31f292
Mohameddiv <84381702+Mohameddiv@users.noreply.github.com>
2025-09-20 21:02:26 +0300

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 Dir="$HOME/Recon/subdomains"
 
-if [[ "$2" == "-save" ]]; then
+if [[ "$2" == "-save" ]]; thensubMonit88r -l 
     #read -p "Enter your domain:" 1
 #read $1
 subfinder -d $1 -all -silent | grep "$1"| anew |sort >>$Dir/$1.txt
 findomain -t $1  -c /home/mohamed/.config/findomain/findomain-config.yml| grep "$1"|grep -v "Target =="| anew |sort >> $Dir/$1.txt
 #amass enum -passive -norecursive -noalts -d $1 | grep "$domain" | anew |sort >> $Dir/$1.txt
-rsescan -d $1 | grep "$1" -key 45caaddd-b3fd-45fa-b70f-3f03ed9eaa81 | anew |sort >> $Dir/$1.txt
+rsescan -d $1 -key 45caaddd-b3fd-45fa-b70f-3f03ed9eaa81 | grep "$1"  | anew |sort >> $Dir/$1.txt
 curl -s https://api.subdomain.center/?domain=$1|jq .|cut -d '"' -f 2 | grep "$1"| anew |sort >> $Dir/$1.txt
 assetfinder -subs-only $1 | grep "$1" | anew |sort >> $Dir/$1.txt
 curl https://shrewdeye.app/domains/$1.txt | grep "$1" | anew |sort >> $Dir/$1.txt
@@ -16,18 +16,20 @@ subMonit88r -l target
 cat subMonit88rResults.txt |  grep "$1"|anew |sort >> $Dir/$1.txt
 rm -rf target subMonit88rResults.txt
 curl -s "https://jldc.me/anubis/subdomains/$1?" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u| anew |sort >> $Dir/$1.txt
-wget https://www.dnsgrep.cn/subdomain/$1 --header "Host: www.dnsgrep.cn"  --referer=https://www.dnsgrep.cn/  --header "Cookie: cf_clearance=6OCDoX9gY46cS_hBkQewhRT9.N548GiR5uuXEAZ3fH0-1690492547-0-250.0.0; cf_chl_2=195f392158c0c53" --user-agent "Mozilla/5.0 (Windows NT 10.0; rv:102.0) Gecko/20100101 Firefox/102.0" -O dnsgrep.txt
-cat dnsgrep.txt|grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u| anew |sort >> $Dir/$1.txt
-rm dnsgrep.txt
+#curl --path-as-is -i -s -k -X $'GET' \
+#    -H $'Host: www.dnsgrep.cn' -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0' \
+#    -b $'cf_clearance=rcLouItY8p9UnP3TFM7huza_wDRD_879yp..31tRO7o-1758386794-1.2.1.1-4hZoA2jfNzD85dfnt7v3Yn4uJP_duv7wQgc4bzQsLOOYNpSlAswoDu4yiMhNv96J4UkrmUOVwpy2FMDCkEsMxvP1shtvWzRsGjHZ14VX_JuNL.c82rCY72V3cWlEqWdT6nlECZ9tSSUO2A7RTjrem6YX7Ooa1A.7tZL4FAI_eds1ttiaCsrbQFoqzEUdLU1w3RoaU6vY1X5jMHbYToR7GqiNrolwFaecJlpOSX0isWs' \
+#    $"https://www.dnsgrep.cn/subdomain/$1" |grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u| anew |sort >> $Dir/$1.txt
 
-echo "$1"|haktrails subdomains | anew |sort >> $Dir/$1.txt
+#echo "$1"|haktrails subdomains | anew |sort >> $Dir/$1.txt
 curl -s "https://api.hackertarget.com/hostsearch/?q=$1" | grep -o "\w.*$1"| anew |sort >> $Dir/$1.txt
-curl https://api.securitytrails.com/v1/domain/$1/subdomains --header 'APIKEY: h7kMNSWW0jURChgflpF4z9gZhdcmzRgL'| jq .subdomains 2>/dev/null | awk -v domain=$1 -F\" '{print $2 "." domain}' | sed '$d' | sed '1d' | anew |sort >> $Dir/$1.txt
+curl https://api.securitytrails.com/v1/domain/$1/subdomains --header 'APIKEY: hJp42gwvt8TpbzTqp7TqpN9_AzJdznmU'| jq .subdomains 2>/dev/null | awk -v domain=$1 -F\" '{print $2 "." domain}' | sed '$d' | sed '1d' | anew |sort >> $Dir/$1.txt
+curl "https://sub-scan-api.reverseipdomain.com/?domain=$1"|jq|cut -d '"' -f2|grep "$1" | sort -u| anew |sort >> $Dir/$1.txt
 
 firebase.sh $1 |grep "\.$1" | anew |sort >> $Dir/$1.txt
 
 curl -s "https://otx.alienvault.com/api/v1/indicators/domain/$1/passive_dns" | jq|grep '"hostname":'|cut -d '"' -f 4 | grep "\.$1"| anew |sort >> $Dir/$1.txt
-#python3 /mnt/sda2/Tools/tools/Sublist3r/sublist3r.py -d $1 | grep "$1" |grep -v "Enumerating subdomains"|anew |sort >> $Dir/$1.txt 
+python3 /home/mohamed/Tools/Sublist3r/sublist3r.py -d $1 | grep "$1" |grep -v "Enumerating subdomains"|anew |sort >> $Dir/$1.txt 
 #gobuster dns -d $1  -w /usr/share/wordlists/subdomain.txt -q | grep "$1"| anew |sort >> $Dir/$1.txt
 crt.sh $1 | grep "$1" | anew |sort >> $Dir/$1.txt 
 #python3 /mnt/sda2/Tools/github-search/github-subdomains.py -t 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 -d $1 | grep "$1"| anew |sort >> $Dir/$1.txt
